e04d4d05e6378a75babee8927ff521e4a57ff885
JordanSgrigna <kaliwyrdiscord@gmail.com>
2023-06-08 11:17:18 +0200

    Get CreateAuto Post CreateAuto, Marca, Modello, Tipo

--- WebAppCarrozzeriaAuto/Controllers/AdminController.cs
+++ WebAppCarrozzeriaAuto/Controllers/AdminController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using WebAppCarrozzeriaAuto.Database;
+using WebAppCarrozzeriaAuto.Models;
+using WebAppCarrozzeriaAuto.Models.ModelsPerViews;
 
 namespace WebAppCarrozzeriaAuto.Controllers
 {
@@ -7,6 +11,116 @@ namespace WebAppCarrozzeriaAuto.Controllers
         public IActionResult PannelloAdmin()
         {
             return View();
+        }
+
+        [HttpGet]
+        [Authorize(Roles = "ADMIN")]
+        public IActionResult CreateAuto()
+        {
+            using (ConcessionarioContext db  = new ConcessionarioContext())
+            {
+                List<Tipo> tipi = db.Tipi.ToList();
+                List<Marca> marche = db.Marche.ToList();
+                List<Modello> modelli = db.Modelli.ToList();
+
+                ModelloMacchinaComplesso modelloPerView = new ModelloMacchinaComplesso();
+                modelloPerView.Auto = new Auto();
+                modelloPerView.Modello = modelli;
+                modelloPerView.Tipo = tipi;
+                modelloPerView.Marca = marche;
+
+                return View(modelloPerView);
+
+            }
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "ADMIN")]
+        public IActionResult CreateAuto(ModelloMacchinaComplesso data)
+        {
+            if (!ModelState.IsValid)
+            {
+                using(ConcessionarioContext db =new ConcessionarioContext())
+                {
+                    List<Tipo> tipi = db.Tipi.ToList();
+                    List<Marca> marche = db.Marche.ToList();
+                    List<Modello> modelli = db.Modelli.ToList();
+
+                    data.Tipo = tipi;
+                    data.Marca = marche;
+                    data.Modello = modelli;
+
+                    return View(data);
+                }
+            }
+            else
+            {
+                using (ConcessionarioContext db = new ConcessionarioContext())
+                {
+                    db.Auto.Add(data.Auto);
+                    db.SaveChanges();
+
+                    return RedirectToAction("Index", "Auto");
+                }
+            }
+        }
+
+        [HttpPost]
+        public IActionResult CreateTipo(Tipo tipo)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("CreateAuto", tipo);
+            }
+            else
+            {
+                using (ConcessionarioContext db = new ConcessionarioContext())
+                {
+                    db.Tipi.Add(tipo);
+                    db.SaveChanges();
+                    return RedirectToAction("CreateAuto", "Admin");
+                }
+            }
+        }
+
+        [HttpPost]
+        public IActionResult CreateModello (Modello modello)
+        {
+            using (ConcessionarioContext db = new ConcessionarioContext())
+            {
+                //PRENDO LA LISTA DEI TIPI DA ASSOCIARE AL MODELLO
+                List<Tipo> tipi = db.Tipi.ToList();
+
+                if (!ModelState.IsValid)
+                {
+                    return View("CreateAuto", modello);
+                }
+                else
+                {
+                    db.Modelli.Add(modello);
+                    db.SaveChanges();
+                    return RedirectToAction("CreateAuto", "Admin");
+
+                }
+            }
+        }
+
+        [HttpPost]
+        public IActionResult CreateMarca (Marca marca)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("CreateAuto", marca);
+            }
+            else
+            {
+                using (ConcessionarioContext db = new ConcessionarioContext())
+                {
+                    db.Marche.Add(marca);
+                    db.SaveChanges();
+                    return RedirectToAction("CreateAuto", "Admin");
+                }
+            }
         }
 
     }
--- WebAppCarrozzeriaAuto/Database/ConcessionarioContext.cs
+++ WebAppCarrozzeriaAuto/Database/ConcessionarioContext.cs
@@ -7,13 +7,13 @@ namespace WebAppCarrozzeriaAuto.Database
 {
     public class ConcessionarioContext : IdentityDbContext<IdentityUser>
     {
-        DbSet<Tipo> Tipi { get; set; }
-        DbSet<Modello> Modelli { get; set; }
-        DbSet<Marca> Marche { get; set; }
-        DbSet<Auto> Auto { get; set; }
-        DbSet<SpecificheTecniche> SpecificheTecniche { get; set; }
-        DbSet<AcquisizioneAuto> AcquisizioniAuto { get; set; }
-        DbSet<VenditaAutoUtente> VenditeAuto { get; set; }
+        public DbSet<Tipo> Tipi { get; set; }
+        public DbSet<Modello> Modelli { get; set; }
+        public DbSet<Marca> Marche { get; set; }
+        public DbSet<Auto> Auto { get; set; }
+        public DbSet<SpecificheTecniche> SpecificheTecniche { get; set; }
+        public DbSet<AcquisizioneAuto> AcquisizioniAuto { get; set; }
+        public DbSet<VenditaAutoUtente> VenditeAuto { get; set; }
 
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
