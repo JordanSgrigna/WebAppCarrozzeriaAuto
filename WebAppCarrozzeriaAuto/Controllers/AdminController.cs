e79d60309806dbd3cddc39ff199ddcabc21a18db
JordanSgrigna <kaliwyrdiscord@gmail.com>
2023-06-08 10:13:02 +0200

    Controller Admin

---
+++ WebAppCarrozzeriaAuto/Controllers/AdminController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAppCarrozzeriaAuto.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult PannelloAdmin()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+
+        }
+    }
+}
